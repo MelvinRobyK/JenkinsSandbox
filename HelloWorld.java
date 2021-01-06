5a0eb9b680f9aab4aff8adce34146a3cab278457
Kariyampallil <MelvinRoby.Kariyampallil@elektrobit.com>
2021-01-06 12:00:59 +0530

    HelloWorld java file added

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! This java file is fetched from the following gutHub link : https://github.com/MelvinRobyK/JenkinsSandbox.git"); 
+    }
+}
